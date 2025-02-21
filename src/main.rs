7fd2cdf2fc0a093a20e5cbb9a21b8ea7653f0865
Mieszko Di Gregorio Botek <mieszkodgb@gmail.com>
2025-02-22 00:12:36 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use axum::{response::Html, routing::get, Router};
+use tokio::net::TcpListener;
+
+#[allow(unused)]
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("Hello <strong> World!!!</strong>")}),
+    );
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+	println!("LISTENING on {:?}\n", listener.local_addr());
+	axum::serve(listener, routes_hello.into_make_service())
+        .await
+        .unwrap();
+}
