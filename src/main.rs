7e1d432c8ab7ae7a7d72b377d2acee928a657403
Mieszko Di Gregorio Botek <mieszkodgb@gmail.com>
2025-02-22 10:44:54 +0100

    add endpoint with param

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{extract::Query, response::{Html, IntoResponse}, routing::get, Router};
+use serde::Deserialize;
 use tokio::net::TcpListener;
 
 #[allow(unused)]
@@ -7,11 +8,24 @@ use tokio::net::TcpListener;
 async fn main() {
     let routes_hello = Router::new().route(
         "/hello",
-        get(|| async { Html("Hello <strong> World!!!</strong>")}),
+        get(handler_hello),
     );
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
 	println!("LISTENING on {:?}\n", listener.local_addr());
 	axum::serve(listener, routes_hello.into_make_service())
         .await
         .unwrap();
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams{
+    name: Option<String>
+}
+
+// e.g. '/hello?params=Mike'
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse{
+    println!("Handler hello - {params:?}");
+
+    let name = params.name.as_deref().unwrap_or("World!");
+    Html(format!("Hello <strong>{name}</strong>"))
 }
