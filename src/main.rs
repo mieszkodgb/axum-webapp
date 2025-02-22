04f5ed6a0cb9f7cacf655f501925b874e98229b4
Mieszko Di Gregorio Botek <mieszkodgb@gmail.com>
2025-02-22 11:30:52 +0100

    merge routes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{extract::{Path, Query}, response::{Html, IntoResponse}, routing::get, Router};
+use axum::{extract::{Path, Query}, response::{Html, IntoResponse}, routing::{get}, Router};
 use serde::Deserialize;
 use tokio::net::TcpListener;
 
@@ -6,14 +6,19 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new()
-        .route("/hello", get(handler_hello))
-        .route("/hello2/{name}", get(handler_hello2));
+    let routes_all = Router::new().merge(routes_hello());
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
 	println!("LISTENING on {:?}\n", listener.local_addr());
-	axum::serve(listener, routes_hello.into_make_service())
+	axum::serve(listener, routes_all.into_make_service())
         .await
         .unwrap();
+}
+
+
+fn routes_hello() -> Router{
+    Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/{name}", get(handler_hello2))
 }
 
 #[derive(Debug, Deserialize)]
