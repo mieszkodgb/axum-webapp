e6674308f0f985795abcebbde63e30ea9be31622
Mieszko Di Gregorio Botek <mieszkodgb@gmail.com>
2025-02-22 22:20:50 +0100

    add tests

--- tests/client.rs
+++ tests/client.rs
@@ -1,5 +1,8 @@
 #![allow(unused)]
 
+use axum::Json;
+use serde_json::{json, Value};
+
 
 #[tokio::test]
 async fn client() -> Result<(),reqwest::Error>{
@@ -12,5 +15,47 @@ async fn client() -> Result<(),reqwest::Error>{
     let body = res.text().await?;
 
     println!("{body}");
+
+    //Hello2
+    let res = reqwest::get("http://localhost:8080/hello2/Mike").await?;
+    println!("Response: {:?} {}", res.version(), res.status());
+    println!("Headers: {:#?}\n", res.headers());
+
+    let body = res.text().await?;
+
+    println!("{body}");
+
+
+    //Static page
+    let res = reqwest::get("http://localhost:8080/src/main.rs").await?;
+    println!("Response: {:?} {}", res.version(), res.status());
+    println!("Headers: {:#?}\n", res.headers());
+
+    let body = res.text().await?;
+
+    println!("{body}");
+
+    //Login
+    let res_login = client.post("http://localhost:8080/api/login")
+        .json(&json!({"username": "testuser", "password": "test123"}))
+        .send().await?;
+    println!("Response: {:?} {}", res_login.version(), res_login.status());
+    println!("Headers: {:#?}\n", res_login.headers());
+
+    let body = res_login.text().await?;
+
+    println!("{body}");
+
+    //Wrong login
+    let res_login = client.post("http://localhost:8080/api/login")
+        .json(&json!({"username": "testuser", "password": "test12"}))
+        .send().await?;
+    println!("Response: {:?} {}", res_login.version(), res_login.status());
+    println!("Headers: {:#?}\n", res_login.headers());
+
+    let body = res_login.text().await?;
+
+    println!("{body}");
+
     Ok(())
 }
